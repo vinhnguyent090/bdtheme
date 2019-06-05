f3f43146591fecfebce39ab308caf9908d1e9796
MTM IT <it@mtm-t.com>
2019-06-05 18:04:36 +0800

    Try to let it work on v11.1.32

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 # -*- coding: utf-8 -*-
+#from setuptools import setup, find_packages
+#from pip.req import parse_requirements
+#import re, ast
+
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 import re, ast
 
-# get version from __version__ variable in bdtheme/__init__.py
+with open('requirements.txt') as f:
+        install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in mtmlogistic/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('bdtheme/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+        version = str(ast.literal_eval(_version_re.search(
+                f.read().decode('utf-8')).group(1)))
 
-requirements = parse_requirements("requirements.txt", session="")
+# get version from __version__ variable in bdtheme/__init__.py
+#_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+#with open('bdtheme/__init__.py', 'rb') as f:
+#    version = str(ast.literal_eval(_version_re.search(
+#        f.read().decode('utf-8')).group(1)))
+#
+#requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='bdtheme',
@@ -21,6 +34,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires
 )
