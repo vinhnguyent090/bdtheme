4ba2eaff9cd8fda0d0ac99334f714d7621573525
MTM IT <it@mtm-t.com>
2019-06-05 18:10:45 +0800

    Cleaned setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-#from setuptools import setup, find_packages
-#from pip.req import parse_requirements
-#import re, ast
 
 from setuptools import setup, find_packages
 import re, ast
@@ -9,21 +6,12 @@ import re, ast
 with open('requirements.txt') as f:
         install_requires = f.read().strip().split('\n')
 
-# get version from __version__ variable in mtmlogistic/__init__.py
+# get version from __version__ variable in bdtheme/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('bdtheme/__init__.py', 'rb') as f:
         version = str(ast.literal_eval(_version_re.search(
                 f.read().decode('utf-8')).group(1)))
-
-# get version from __version__ variable in bdtheme/__init__.py
-#_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-#with open('bdtheme/__init__.py', 'rb') as f:
-#    version = str(ast.literal_eval(_version_re.search(
-#        f.read().decode('utf-8')).group(1)))
-#
-#requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='bdtheme',
